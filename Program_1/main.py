9d09ec393798af040b78e1bad78de87708336908
tdhaxton <tdhaxton1223@my.msutexas.edu>
2024-07-22 11:42:04 -0500

    22 July morning, Line 46-47 issues

--- Program_1/main.py
+++ Program_1/main.py
@@ -7,7 +7,7 @@ tweets = []
 
 #pull tweets out of .txt file
 #open the file
-with open("/home/haxtontd/Projects/4553-NLP/Climate_change_2022-1-17_2022-7-19.txt", "r") as file:
+with open("/home/haxtontd/projects/school/4553-NLP/Climate_change_2022-1-17_2022-7-19.txt", "r") as file:
     content = file.read()
     #open file to write comments out to
     with open("output.txt", "w") as file2:
@@ -16,6 +16,7 @@ with open("/home/haxtontd/Projects/4553-NLP/Climate_change_2022-1-17_2022-7-19.t
         file2.write(str(comments))
 
 print(len(comments))
+sample_size = len(comments)
 
 #write comments to list
 for comment in comments:
@@ -28,6 +29,25 @@ print(len(tweets))
 
 #create a SentimentIntensityAnalyzer object
 sia_obj = SentimentIntensityAnalyzer()
+file_path = "vaderOutput.txt"
+file3 = open(file_path, "w")
+
+sentiment_analyses = []
+
 for tweet in tweets:
     answer = sia_obj.polarity_scores(tweet)
-    print(answer)
+    file3.write(str(answer))
+    file3.write("\n")
+    sentiment_analyses.append(answer)
+    #print(answer)
+
+#compile compound polarity scores
+total = 0
+for scores in sentiment_analyses:
+    total += sentiment_analyses[scores]
+
+#calculate average sentiment
+avg_sentiment = total / len(tweets)
+print(avg_sentiment)
+
+file3.close()
